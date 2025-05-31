12f45fcab65f6a56bab246d2977b8cf330508e37
Hari Vinod <harivinod1018@gmail.com>
2025-05-31 18:17:25 +0530

    Fix model 1

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const Post = require("../models/Post");
+const Post = require("../models/Post.js");
 const fs = require("fs");
 const path = require("path");
 
